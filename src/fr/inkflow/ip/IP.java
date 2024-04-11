025a5638aac4627c444acf7b193a221f2d4a7b5c
Inkflow59 <101215221+Inkflow59@users.noreply.github.com>
2024-04-11 14:43:43 +0200

    Ajout de la méthode decimPointee

--- src/fr/inkflow/ip/IP.java
+++ src/fr/inkflow/ip/IP.java
@@ -1,5 +1,7 @@
 package fr.inkflow.ip;
 
+import java.util.Scanner;
+
 public class IP {
     private int oct1, oct2, oct3, oct4;
 
@@ -8,6 +10,15 @@ public class IP {
         this.oct2 = oct2;
         this.oct3 = oct3;
         this.oct4 = oct4;
+    }
+
+    public IP() {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your IP address (without the mask), separated by 'Enter'");
+        this.oct1=input.nextInt();
+        this.oct2=input.nextInt();
+        this.oct3=input.nextInt();
+        this.oct4=input.nextInt();
     }
 
     public int getOct1() {
@@ -40,5 +51,10 @@ public class IP {
 
     public void setOct4(int oct4) {
         this.oct4 = oct4;
+    }
+
+    public String decimPointee() {
+        String ipString=this.oct1+"."+this.oct2+"."+this.oct3+"."+this.oct4;
+        return ipString;
     }
 }
---
+++ src/fr/inkflow/ip/Main.java
@@ -0,0 +1,8 @@
+package fr.inkflow.ip;
+
+public class Main {
+    public static void main(String[] args) {
+        IP ip=new IP(192,168,1,2);
+        System.out.println(ip.decimPointee());
+    }
+}
